dfbe7075e7eab3fbb00bd2ad91dde0c9b99e4854
Unzueta <unzferk@gmail.com>
2018-04-28 00:31:31 -0400

    funciona+css+bootstrap

--- resources/views/home.php
+++ resources/views/home.php
@@ -3,34 +3,49 @@
 <head>
     <meta charset="UTF-8">
     <title>IRBE</title>
+    <link rel="stylesheet" href="plugin/bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="icon/style.css">
     <link rel="stylesheet" href="css/estilos.css">
 </head>
 <body>
     <main>
         <div class="content-menu">
-            
-            <li><span class="lnr lnr-home icon1"></span><h4 class="text1">Inicio</h4></li>
-            <li><span class="lnr lnr-film-play icon2"></span><h4 class="text2">Videos</h4></li>
-            <li><span class="lnr lnr-store icon3"></span><h4 class="text3">Productos</h4></li>
-            <li><span class="lnr lnr-picture icon4"></span><h4 class="text4">Galeria</h4></li>
-            <li><span class="lnr lnr-briefcase icon5"></span><h4 class="text5">Foro</h4></li>
-            <li><span class="lnr lnr-license icon6"></span><h4 class="text6">Blog</h4></li>
-            <li><span class="lnr lnr-bubble icon7"></span><h4 class="text7">Mensajes</h4></li>
-            <li><span class="lnr lnr-envelope icon8"></span><h4 class="text8">Contactos</h4></li>
-            <li><span class="lnr lnr-question-circle icon9"></span><h4 class="text9">Nosotros</h4></li>
-            
+            <li><span class="lnr lnr-home icon1"></span><h6 class="text1">Inicio</h6></li>
+            <li><span class="lnr lnr-film-play icon2"></span><h6 class="text2">Videos</h6></li>
+            <li><span class="lnr lnr-store icon3"></span><h6 class="text3">Productos</h6></li>
+            <li><span class="lnr lnr-picture icon4"></span><h6 class="text4">Galeria</h6></li>
+            <li><span class="lnr lnr-briefcase icon5"></span><h6 class="text5">Foro</h6></li>
+            <li><span class="lnr lnr-license icon6"></span><h6 class="text6">Blog</h6></li>
+            <li><span class="lnr lnr-bubble icon7"></span><h6 class="text7">Mensajes</h6></li>
+            <li><span class="lnr lnr-envelope icon8"></span><h6 class="text8">Contactos</h6></li>
+            <li><span class="lnr lnr-question-circle icon9"></span><h6 class="text9">Nosotros</h6></li>
         </div>
         
+
         
         <div class="contend-main">
+            
+            <div class="container-fluid titus">
+                <div class="row">
+                    <!--div class="col-md-2 cent">
+                        <span class="lnr lnr-menu show"></span> 
+                    </div>
+                    <div class="col-md-10 logo-cent">
+                        <img src="img/irrbe.png" class="img-responsive">
+                    </div-->
+                    <span class="lnr lnr-menu show"></span>
+                    <img src="img/irrbe.png" class="img-responsive" width="440" height="80" style="margin:0 auto;"> 
+                </div>
+            </div>
+
             <div class="titus">
-                <span class="lnr lnr-menu show"></span> 
+                
             </div>   
             
 
             <div class="mcontainer">
             <h1>Bienvenido</h1>
+
             
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis consectetur quia asperiores eligendi minima doloribus neque nemo temporibus repellendus fugiat inventore quaerat pariatur obcaecati nulla ab laudantium, vel laborum libero. Illum officiis rem, repellendus aliquid tempora sit saepe vel odio porro iure laudantium architecto ipsum, fuga dicta quod maxime? Ullam qui quis cum eaque omnis nesciunt ex a optio quos dolor sint sed ab modi, doloribus. Minima at quisquam consequatur. Voluptatum quaerat temporibus, perferendis id ab, debitis inventore voluptatibus molestias? Ex reiciendis dolorem, consequatur voluptas. Et labore earum, quia sed ea cupiditate molestias aliquam sapiente officia quibusdam repellendus, recusandae perspiciatis aperiam temporibus. Quasi, temporibus beatae neque deserunt eligendi omnis error sequi nam ullam doloremque perferendis doloribus, consectetur illo deleniti, sit tenetur. Libero odio quaerat pariatur officiis eius minus, suscipit perspiciatis eos ratione adipisci, voluptate facere cum excepturi, assumenda natus exercitationem. Quas et harum ducimus aliquam tempora nihil. Nulla veritatis perspiciatis quas porro quae voluptas in, eligendi dignissimos facilis cum. Qui ipsa nobis soluta aliquid cupiditate cumque, in perspiciatis, molestias possimus ex rerum, nostrum. Ea nisi dicta, animi reiciendis reprehenderit architecto voluptatem ipsum repudiandae similique modi, est maiores molestiae necessitatibus blanditiis mollitia porro aliquid ad. Voluptate ut dignissimos necessitatibus molestias pariatur?</p>
             
@@ -43,6 +58,8 @@
         </div>
     </main>
     
+
+    <script src="plugin/bootstrap/js/bootstrap.js"></script>
     <script src="js/jquery.js"></script>
     <script src="js/script.js"></script>
 </body>
